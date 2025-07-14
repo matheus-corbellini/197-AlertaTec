6c7333c45e17fe1f89fefc6ed3c7b794eadfcfc7
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-14 11:13:26 -0300

    modificacoes iniciais

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEdUKQ8bRW56CAB5UpbG-lj32XG1dkqAg",
+  authDomain: "alertatec-da6ce.firebaseapp.com",
+  projectId: "alertatec-da6ce",
+  storageBucket: "alertatec-da6ce.firebasestorage.app",
+  messagingSenderId: "774565431078",
+  appId: "1:774565431078:web:95b822d829ef2dbac963e7",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
